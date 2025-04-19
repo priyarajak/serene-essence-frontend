08f0af8564c7bda0d71b564d6032d10de1612786
PRIYA RAJAK <72929067+priyarajak@users.noreply.github.com>
2025-04-19 19:55:07 +0530

    Update LoginPage.js

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -15,7 +15,7 @@ export default function LoginPage() {
   const handleLogin = async (e) => {
     e.preventDefault();
 
-    const response = await fetch('http://localhost:5050/login', {
+    const response = await fetch("https://serene-essence-backend.onrender.com/login"), {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ email, password }),
